8eda970e499a109f183171aab6716109813185a2
Ron Juarez <ronstewartjuarez@gmail.com>
2020-04-10 09:23:45 -0400

    this is the commit for the second part of the iss spotter assignment

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
-fetchMyIP ((error, ip) => {
+// fetchMyIP ((error, ip) => {
   
+//   if (error) {
+//     console.log(error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+
+fetchCoordsByIP('142.118.116.81', (error, coords) => {
   if (error) {
-    console.log(error);
+    console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned IP:' , ip);
+  console.log('It worked! Returned Coords:' , coords);
 });
--- iss.js
+++ iss.js
@@ -2,19 +2,35 @@ const request = require('request');
 
 
 
-const fetchMyIP = (callback) => {
-  request('https://api.ipify.org?format=json', (error, response, body) => {
-    const ip = JSON.parse(body).ip;
+// const fetchMyIP = (callback) => {
+//   request('https://api.ipify.org?format=json', (error, response, body) => {
+//     const ip = JSON.parse(body).ip;
 
-    callback(error,ip);
+//     callback(error,ip);
 
-  })
-  
-  const fetchMyIpCoords = (ip, callback) => {
-  request(`https://ipvigilante.com/json/${ip}`(error, response, body))
+//   })
+// }  
 
 
-  }
+const fetchCoordsByIP = ((ip, callback) => {
+  request(`https://ipvigilante.com/json/${ip}`, (error, response, body) => {
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body).data;
+
+    callback(null, { latitude, longitude });
+})
+});
+
 
 //     if (error) {
 //       console.log('error:', error); 
@@ -22,9 +38,8 @@ const fetchMyIP = (callback) => {
 //     } // Print the error if one occurred
 //     console.log('statusCode:', ip); // Print the response status code if a response was received
 // //    console.log('body:', body); // Print the HTML for the Google homepage.
-  };
+
 
  
-
-
-module.exports = { fetchMyIP };
+module.exports = { fetchCoordsByIP };
+//module.exports = { fetchMyIP };
